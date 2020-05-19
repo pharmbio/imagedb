ca7d178737e039daf518ad91003b33ce69c657f8
andersla <anders@ormbunkar.se>
2020-05-19 14:45:02 +0000

    update filename parsing for new project/platename

--- cli/filenames/pharmbio_filename_v1.py
+++ cli/filenames/pharmbio_filename_v1.py
@@ -8,12 +8,14 @@ import logging
 
 __pattern_path_and_file   = re.compile('^'
                             + '.*'        # any
-                            + '([0-9]{4})-([0-9]{2})-([0-9]{2})' # date (yyyy, mm, dd) (1,2,3)
+                            + 'MDC_pharmbio/(.*?)/' # project (1)
+                            + '(.*?)/' # plate (2)
+                            + '([0-9]{4})-([0-9]{2})-([0-9]{2})' # date (yyyy, mm, dd) (3,4,5)
                             + '.*'        # Any
                             + 'TimePoint_([^\/]+)' # Timepoint (4)
-                            + '\/([^-]+)' # project-name (5)
+                            + '\/([^-]+)' # cell-line-name (5)
                             + '-([^-]+)'  # magnification (6)
-                            + '-([^_]+)'  # plate (7)
+                            + '-([^_]+)'  # plate-short (7)
                             + '_([^_]+)'  # well (8)
                             + '_s([0-9])'  # wellsample (9) # OBS! Only 9 wellsamples
                             + '(_w[0-9])?' # optional channel (10)
@@ -35,28 +37,34 @@ def parse_path_and_file(path):
   # parse channel
   # first set default
   channel = 1
-  if match.group(10):
+  if match.group(12):
     # remove _w from match
-    channel = match.group(10)[2]
+    channel = match.group(12)[1]
+
 
   metadata = {
     'path': path,
     'filename': os.path.basename(path),
-    'date_year': int(match.group(1)),
-    'date_month': int(match.group(2)),
-    'date_day_of_month': int(match.group(3)),
-    'timepoint': int(match.group(4)),
-    'project': match.group(5),
-    'magnification': match.group(6),
-    'plate': match.group(7),
-    'well': match.group(8),
-    'wellsample': match.group(9),
+    'date_year': int(match.group(3)),
+    'date_month': int(match.group(4)),
+    'date_day_of_month': int(match.group(5)),
+    'timepoint': int(match.group(6)),
+    'project': match.group(1),
+    'magnification': match.group(8),
+    'plate': match.group(2),
+    'well': match.group(10),
+    'wellsample': match.group(11),
     'channel': channel,
-    'is_thumbnail': match.group(11) is not None,
-    'guid': match.group(12),
-    'extension': match.group(13),
+    'is_thumbnail': match.group(13) is not None,
+    'guid': match.group(14),
+    'extension': match.group(15),
   }
 
   return metadata
+
+if __name__ == '__main__':
+    # Testparse
+    retval = parse_path_and_file("/share/mikro/IMX/MDC_pharmbio/exp-TimeLapse/A549-20X-DB-HD-BpA-pilot1/2019-03-27/84/TimePoint_1/A549-20X-DB-HD-BpA-pilot1_B02_s1_thumb1E64F2F4-E1E8-410C-9891-A491D91FC73C.tif")
+    print("retval = " + str(retval))
 
 
--- cli/filenames/pharmbio_filename_v2.py
+++ cli/filenames/pharmbio_filename_v2.py
@@ -10,20 +10,22 @@ import logging
 
 __pattern_path_and_file   = re.compile('^'
                             + '.*'        # any
-                            + '([0-9]{4})-([0-9]{2})-([0-9]{2})' # date (yyyy, mm, dd) (1,2,3)
+                            + 'MDC_pharmbio/(.*?)/' # project (1)
+                            + '(.*?)/' # plate (2)
+                            + '([0-9]{4})-([0-9]{2})-([0-9]{2})' # date (yyyy, mm, dd) (3,4,5)
                             # TODO could check for optional timepoint here
                             # Now instead the timepoint version is being tested first
                             + '.*\/'      # any until last /
-                            + '([0-9]{6}-)?' # maybe date here also (4)
-                            + '([^-]+)'   # project-name (5)
-                            + '-([^-]+)'  # magnification (6)
-                            + '-([^_]+)'  # plate (7)
-                            + '_([^_]+)'  # well (8)
-                            + '_s([^_]+)'  # wellsample (9)
-                            + '_w([0-9]+)' # Channel (color channel?) (10)
-                            + '(_thumb)?'  # Thumbnail (11)
-                            + '([A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12})'  # Image GUID [12]
-                            + '(\.tiff?)?'  # Extension [13]
+                            + '([0-9]{6}-)?' # maybe date here also (6)
+                            + '([^-]+)'   # project-name (7)
+                            + '-([^-]+)'  # magnification (8)
+                            + '-([^_]+)'  # plate-short (9)
+                            + '_([^_]+)'  # well (10)
+                            + '_s([^_]+)'  # wellsample (11)
+                            + '_w([0-9]+)' # Channel (color channel?) (12)
+                            + '(_thumb)?'  # Thumbnail (13)
+                            + '([A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12})'  # Image GUID [14]
+                            + '(\.tiff?)?'  # Extension [15]
                             + '$'
                             ,
                             re.IGNORECASE)  # Windows has case-insensitive filenames
@@ -37,21 +39,26 @@ def parse_path_and_file(path):
   metadata = {
     'path': path,
     'filename': os.path.basename(path),
-    'date_year': int(match.group(1)),
-    'date_month': int(match.group(2)),
-    'date_day_of_month': int(match.group(3)),
-    'project': match.group(5),
-    'magnification': match.group(6),
-    'plate': match.group(7),
-    'well': match.group(8),
-    'wellsample': match.group(9),
-    'channel': int(match.group(10)),
-    'is_thumbnail': match.group(11) is not None,
-    'guid': match.group(12),
-    'extension': match.group(13),
+    'date_year': int(match.group(3)),
+    'date_month': int(match.group(4)),
+    'date_day_of_month': int(match.group(5)),
+    'project': match.group(1),
+    'magnification': match.group(8),
+    'plate': match.group(2),
+    'well': match.group(10),
+    'wellsample': match.group(11),
+    'channel': int(match.group(12)),
+    'is_thumbnail': match.group(13) is not None,
+    'guid': match.group(14),
+    'extension': match.group(15),
     'timepoint': 1,
   }
 
   return metadata
+
+if __name__ == '__main__':
+    # Testparse
+    retval = parse_path_and_file("/share/mikro/IMX/MDC_pharmbio/PolinaG-U2OS/181212-U2OS-20X-BpA-HD-DB-high/2018-12-12/1/181212-U2OS-20X-BpA-HD-DB-high_E02_s7_w3_thumbCFB5B241-4E5B-4AB4-8861-A9B6E8F9FE00.tif")
+    print("retval = " + str(retval))
 
 
