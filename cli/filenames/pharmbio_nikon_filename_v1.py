5a9558f764fab218c56c1b3d1043fced0f2e097d
andersla <anders@ormbunkar.se>
2023-02-24 15:39:50 +0100

    added nikon parser

---
+++ cli/filenames/pharmbio_nikon_filename_v1.py
@@ -0,0 +1,85 @@
+import re
+import os
+import logging
+
+# Adopted from: https://github.com/HASTE-project/haste-image-analysis-container2/tree/master/haste/image_analysis_container2/filenames
+#
+# file example
+# /share/mikro/nikon/RMS-test/batch2-RH30/20230223_183729_719__WellI02_PointI02_0002_ChannelFar\ Red\ Single_Seq8699.tiff
+#
+
+__pattern_path_and_file = re.compile('^'
+                                     + '.*/nikon/'      # any until /nikon/
+                                       + '(.*?)/'       # project (1)
+                                       + '(.*?)/'       # plate (2)
+                                       + '([0-9]{4})([0-9]{2})([0-9]{2})' # date (yyyy, mm, dd) (3,4,5)
+                                       + '.*Well([A-Z])([0-9]+)_'         # well (6,7)
+                                       + '.*Point.*_([0-9]+)_'            # site (8)
+                                       + '.*Channel(.*)_Seq'              # channel (9)
+                                       + '.*(\..*)'                       # Extension [10]
+                                     ,
+                                     re.IGNORECASE)  # Windows has case-insensitive filenames
+
+
+def parse_path_and_file(path):
+ # If something errors (file not parsable with this parser, then exception and return None)
+ try:
+  match = re.search(__pattern_path_and_file, path)
+
+  logging.debug(f'match: {match}')
+
+  if match is None:
+    return None
+
+  logging.debug(f'match: {match.groups() }')
+
+  row = match.group(6)
+  col = match.group(7)
+  well = f'{row}{col}'
+
+  channel_name = match.group(9)
+  channels = ['Blue', 'YFP', 'Red', 'Far Red Single', 'FITC']
+  channel_pos = channels.index(channel_name) + 1
+
+  site = int(match.group(8))
+
+  metadata = {
+      'path': path,
+      'filename': os.path.basename(path),
+      'date_year': int(match.group(3)),
+      'date_month': int(match.group(4)),
+      'date_day_of_month': int(match.group(5)),
+      'project': match.group(1),
+      'magnification': '20x',
+      'plate': match.group(2),
+      'plate_acq_name': path,
+      'well': well,
+      'wellsample': site,
+      'channel': channel_pos,
+      'is_thumbnail': False,
+      'guid': None,
+      'extension': match.group(10),
+      'timepoint': 1,
+      'channel_map_id': 10,
+      'microscope': "nikon",
+      'parser': os.path.basename(__file__)
+  }
+
+  return metadata
+
+ except:
+    logging.exception("exception")
+    logging.debug("could not parse")
+    return None
+
+
+if __name__ == '__main__':
+    # Configure logging
+    #
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+
+    retval = parse_path_and_file(
+        "/share/mikro/nikon/RMS-test/batch2-RH30/20230223_183729_719__WellI02_PointI02_0002_ChannelFar Red Single_Seq8699.tiff")
+    print("retval = " + str(retval))
