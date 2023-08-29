bd58ef0b1f2e9bbbfacccef21f6158ec8fd7cb54
andersla <anders@ormbunkar.se>
2023-08-29 12:40:36 +0200

    new Docker container

--- webserver/build-push-docker.sh
+++ webserver/build-push-docker.sh
@@ -1,5 +1,12 @@
-#docker build -t pharmbio/imagedb-web:master .
-#docker push pharmbio/imagedb-web:master
+docker build -t ghcr.io/pharmbio/imagedb-web:devel .
+docker push ghcr.io/pharmbio/imagedb-web:devel
 
-docker build -t ghcr.io/pharmbio/imagedb-web:master .
-docker push ghcr.io/pharmbio/imagedb-web:master
+read -p "Do you want to push with \"stable\" tag also? [y|n]" -n 1 -r < /dev/tty
+echo
+if ! grep -qE "^[Yy]$" <<< "$REPLY"; then
+    exit 1
+fi
+
+docker build -t ghcr.io/pharmbio/imagedb-web:stable .
+docker push ghcr.io/pharmbio/imagedb-web:stable
+
--- webserver/server.py
+++ webserver/server.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-"""
-Simple Tornado server.
-"""
-
 import os
 import logging
 import tornado
